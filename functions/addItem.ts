60931f94c5d75b868e6cf6f58bf1b9eadf70f02c
arturmartins <arturmartinssilva078@gmail.com>
2024-09-05 19:30:02 -0300

    fit: função addItem corrigida.
    Co-authored-by: Kauê Alves Silva <Kaue-Alves@users.noreply.github.com>

--- functions/addItem.ts
+++ functions/addItem.ts
@@ -1,15 +1,16 @@
-import { validar } from "./validar"
 import { adicionaItem } from "../bancoDeDados/pegarArquivo"
+import { Objeto } from "./interfaceObjeto";
+import { validar } from "./validar";
 
 export async function addItem() {
-    const nomeItem = prompt("Nome do Item:")
+    const nomeItem = prompt("Nome do Item:")!
     const quantidade = Number(prompt("Quantidade:"))
-    const categoria = prompt("Categoria:")
+    const categoria = prompt("Categoria:")!
 
     if (validar(nomeItem, quantidade, categoria) == false) {
         console.log("\nAlgum campo está vazio ou inválido, tente novamente preenchendo todos os campos.\n");
     } else {
-        const obj = {
+        const obj: Objeto = {
             nome: nomeItem,
             quantidade: quantidade,
             categoria: categoria,
@@ -17,5 +18,5 @@ export async function addItem() {
         }
         await adicionaItem(obj)
         console.log("\nItem adicionado com sucesso!");
-    }       
+    }      
 }
