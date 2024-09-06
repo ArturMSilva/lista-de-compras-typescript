a3fb91f7576446673acb2e82010ba9ec6fa92f61
arturmartins <arturmartinssilva078@gmail.com>
2024-09-06 15:31:14 -0300

    refactor: migrado arquivo removeItem de .js para .ts fit: função removeItem( ) corrigida
    Co-authored-by: Kauê Alves Silva <Kaue-Alves@users.noreply.github.com>

--- functions/removeItem.ts
+++ functions/removeItem.ts
@@ -2,18 +2,19 @@
 import { FILE_NAME } from "../listaDeCompras";
 import { listaDeCompra } from "../bancoDeDados/pegarArquivo";
 import { exibir } from "./exibir";
+import { Objeto } from "./interfaceObjeto";
 
 export async function removeItem(){
     const lista = await listaDeCompra()
     exibir(lista)
     if (lista.length > 0){
-        const nomeItem = prompt("\nDigite o nome do item que deseja remover:").toLowerCase()
-        const itemIndex = lista.findIndex(lis => lis.nome.toLowerCase() === nomeItem);
+        const nomeItem = prompt("\nDigite o nome do item que deseja remover:")?.toLowerCase()
+        const itemIndex = lista.findIndex((lis: Objeto) => lis.nome.toLowerCase() === nomeItem);
 
         if (itemIndex === -1) {
             return console.log("\nItem não encontrado.")
         }
-        const confirma = prompt("\nTem certeza que quer remover este item?[s - SIM/n - NÃO]").toLowerCase()
+        const confirma = prompt("\nTem certeza que quer remover este item?[s - SIM/n - NÃO]")?.toLowerCase()
         if (itemIndex >= 0 && itemIndex < lista.length && confirma === "s"){
             lista.splice(itemIndex, 1)
             await Bun.write(FILE_NAME, JSON.stringify(lista))
