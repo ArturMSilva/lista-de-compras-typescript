a8581a71cc8da76211707ea0d1f67f96dbe42f4b
arturmartins <arturmartinssilva078@gmail.com>
2024-09-06 14:40:18 -0300

    refactor: migrado arquivo validar de .js para .ts fit: função editarItem( ) corrigida
    Co-authored-by: Kauê Alves Silva <Kaue-Alves@users.noreply.github.com>

--- functions/editarItem.ts
+++ functions/editarItem.ts
@@ -1,26 +1,26 @@
 import { listaDeCompra } from "../bancoDeDados/pegarArquivo";
 import { FILE_NAME } from "../listaDeCompras";
+import { Objeto } from "./interfaceObjeto";
 import { validar } from "./validar"
 
 export async function editarItem () {
     let lista = await listaDeCompra()
-    console.log(lista);
-    console.log("");
-    const nome = prompt("\nDigite o nome do item: ").toLowerCase()
-    const i = lista.findIndex(elemento => {
+    console.table(lista);
+    const nome = prompt("\nDigite o nome do item: ")?.toLowerCase()
+    const i = lista.findIndex((elemento: Objeto) => {
         return (elemento.nome).toLowerCase() === nome
     })
     
     if(i != -1) {
-        const nomeItem = prompt("Digite o novo nome do item: ")
-        const quantidade = Number(prompt("Digite a nova quantidade do item: "))
-        const categoria = prompt("Digite a nova categoria do item: ")
+        const nomeItem = prompt("Digite o novo nome do item: ")!
+        const quantidade = Number(prompt("Digite a nova quantidade do item: "))!
+        const categoria = prompt("Digite a nova categoria do item: ")!
 
         if(validar(nomeItem, quantidade, categoria) == false) {
             console.log("\nAlgum campo está vazio ou inválido, tente novamente preenchendo todos os campos.");
 
-        } else {
-            const obj = {
+} else {
+            const obj: Objeto = {
                 nome: nomeItem,
                 quantidade: quantidade,
                 categoria: categoria,
